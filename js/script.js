3021b00aec96a3af1c603b8449cc465127d92bbc
Vito Gentile <gentilevito1076@gmail.com>
2021-01-15 13:44:34 +0100

    File Preparati

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// TRACCIA:
+// Un alert espone 5 numeri casuali diversi.
+// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+// Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+// Consigli del giorno:
+// * Pensate prima in italiano.
+// * Dividete in piccoli problemi la consegna.
+// * Individuate gli elementi di cui avete bisogno per realizzae il programma.
