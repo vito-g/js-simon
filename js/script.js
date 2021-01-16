e260059a90dfbdc017d46a60b3e7ed6e7d8257b0
Vito Gentile <gentilevito1076@gmail.com>
2021-01-16 09:43:20 +0100

    Generic FX Added: Calcolo numeri "randomici"

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@
 // * Dividete in piccoli problemi la consegna.
 // * Individuate gli elementi di cui avete bisogno per realizzae il programma.
 // -----------------------------------------------------------------------------------------------------------
+
+// STEP 1: Un alert espone 5 numeri casuali diversi.
+
+/*Questo vuol dire che, all'interno dello script avrò bisogno di una FX per il calcolo "randomico" dei numeri, tra un val max (a) ed un val min (b) che andrò a stabilire, arbitrariamente, in fase di richiamo della funzione stessa.
+La funzione è quella che segue:*/
+function random(a, b) {
+  var randomPcNumber = Math.floor((Math.random() * b) + a);
+  return randomPcNumber;//restituirà il valore calcolato, nel punto in cui la funzione verrà richiamata attraverso l'istruzione random(a,b), dove "a" e "b" hanno valori numerici assegnati.
+}
